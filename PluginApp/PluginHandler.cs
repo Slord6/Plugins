ad937d03f76d33e3270dd031ed4187e75112e297
Sam Lord <samlord6@gmail.com>
2020-05-26 19:36:17 +0100

    Add support for loading plugins from remote URIs

--- PluginApp/PluginHandler.cs
+++ PluginApp/PluginHandler.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.Text;
+using System.Web;
 
 namespace PluginApp
 {
@@ -14,6 +16,12 @@ namespace PluginApp
         {
             return pluginPaths.SelectMany(pluginPath =>
             {
+                if(IsRemotePath(pluginPath))
+                {
+                    pluginPath = DownloadPlugin(pluginPath);
+                    if (pluginPath == null) new List<IPlugin>();
+                }
+
                 Assembly pluginAssembly = LoadPluginAssembly(pluginPath);
                 IEnumerable<IPlugin> pluginsFromAssembly = CreateAllPluginsInAssembly(pluginAssembly);
                 foreach (IPlugin plugin in pluginsFromAssembly)
@@ -24,11 +32,64 @@ namespace PluginApp
             }).ToList();
         }
 
+        private static bool IsRemotePath(string path)
+        {
+            try
+            {
+                Uri uri = new Uri(path);
+                return (uri.Scheme == Uri.UriSchemeHttps || uri.Scheme == Uri.UriSchemeHttps);
+            }
+            catch (Exception) {
+                return false;
+            }
+        }
+
+        private static string DownloadPlugin(string path)
+        {
+            Console.WriteLine("Remote plugin - " + path);
+            using (WebClient client = new WebClient())
+            {
+                string fileName = UrlToAssemblyFileName(path);
+                Console.WriteLine("Will save as " + fileName);
+                try
+                {
+                    client.DownloadFile(path, fileName);
+                    return fileName;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to download plugin from " + path);
+                    Console.WriteLine(ex.Message);
+                    return null;
+                }
+            }
+        }
+
+        private static string UrlToAssemblyFileName(string url)
+        {
+            string fileName = HttpUtility.UrlDecode(url)
+                .Split("/", StringSplitOptions.RemoveEmptyEntries).Last()
+                .Split(@"\", StringSplitOptions.RemoveEmptyEntries).Last()
+                .Split("?", StringSplitOptions.RemoveEmptyEntries).First()
+                .Split(".dll", StringSplitOptions.RemoveEmptyEntries).First()
+                +".dll";
+            return @".\Plugins\" + ToValidFilename(fileName);
+        }
+
+        private static string ToValidFilename(string orig)
+        {
+            foreach (char c in Path.GetInvalidFileNameChars())
+            {
+                orig = orig.Replace(c, '_');
+            }
+            return orig;
+        }
+
         public static void RunPlugins(string[] args, IEnumerable<IPlugin> plugins)
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("Commands: ");
+                Console.WriteLine("Loaded Plugins: ");
                 foreach (IPlugin command in plugins)
                 {
                     Console.WriteLine($"{command.Name}\t - {command.Description}");
--- README.md
+++ README.md
@@ -12,5 +12,8 @@
 - `CreateTask` may be called at any time after `OnLoad` and before `OnUnload`. The returned task should implement the desired behaviour of the plugin. `CreateTask` may return a `PluginResultData` object or `null`.
 - `OnUnload` may be called at any time after `OnLoad`. Note that it may be called before `CreateTask`, whilst a task is still running or after it has completed.
 
+## Loading remote plugins
+
+The PluginHandler will accept remote URIs for plugins. The URI must contain the name of the assembly as the last part of the path. eg. `https://example.com/PluginName.dll` or `https://example.com/SearchPlugin.dll?token=abc123`.
 
 Loosely based on the .NET Core plugin system described [here](https://docs.microsoft.com/en-us/dotnet/core/tutorials/creating-app-with-plugin-support)
